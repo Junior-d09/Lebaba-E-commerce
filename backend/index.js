f2aa5ba2df0601b4e1be058dba54b66dabd5b4af
Junior-d09 <christdofonsou@gmail.com>
2024-09-26 15:42:43 +0100

    frontend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+const port = process.env.PORT || 5000;
+
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  
+   
+  }
+
+app.get('/', (req, res) => {
+  res.send('Lebaba E-commerce Server is running...!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
